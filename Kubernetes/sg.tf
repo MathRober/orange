685a19b98e07843be51cc2a962c707c41b03f34b
MathRober <MathRober@users.noreply.github.com>
2020-04-06 16:53:25 +0200

    Update sg.tf

--- Kubernetes/sg.tf
+++ Kubernetes/sg.tf
@@ -19,13 +19,3 @@ resource "aws_security_group_rule" "allow_all_eks" {
   source_security_group_id = aws_security_group.eks_sg.id
   security_group_id        = aws_security_group.eks_sg.id
 }
-#resource "aws_security_group_rule" "ingress_default_eks" {
-#  for_each                  = var.allowed_security_groups
-#
-#  type                     = "ingress"
-#  from_port                = 0
-#  to_port                  = 65535
-#  protocol                 = "-1"
-#  security_group_id        = aws_security_group.eks_sg.id
-#  source_security_group_id  = each.value
-#}
